7d84209cdfb8dddfce55dcc868b0dad79746f04b
unknown <atwkdeveloper@outlook.com>
2019-11-16 21:27:17 +0800

    anagram easy

---
+++ Hackerrank/Easy/anagram.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/anagram/problem
+
+def anagram(s):
+    # Time: O(N) | Space: O(N) | Dictionary
+    if len(s) % 2 == 1:
+        return -1
+    s1 = s[0:len(s)//2]
+    s2 = s[len(s)//2:len(s)]
+    characters = {}
+    additionalCharacters = 0
+    for character in s2:
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    
+    for character in s1:
+        if character in characters:
+            characters[character] -= 1
+            if characters[character] < 0:
+                additionalCharacters += 1
+
+        else:
+            continue
+    return sum(characters.values()) + additionalCharacters
+
+def test_anagram():
+    assert anagram("fdhlvosfpafhalll") == 5, "This should be 5"
+    print("Everything is correct")
+
+test_anagram()
