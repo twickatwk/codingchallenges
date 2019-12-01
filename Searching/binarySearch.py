b3938e55b710337049d168d30d4f76050cc37d4a
unknown <atwkdeveloper@outlook.com>
2019-12-01 13:15:09 +0800

    added iterative binary search

---
+++ Searching/binarySearch.py
@@ -0,0 +1,27 @@
+
+# This is an implementation of binary search using the iterative approach:
+
+def binarySearch(array, target):
+    lower = 0 
+    upper = len(array) - 1
+
+    # Continue this process as long as there is a middle element between the two pointers (left and right)
+    while lower + 1 < upper:
+        distance = (upper - lower) // 2
+        middle = lower + distance
+        if array[middle] == target:
+            return True
+        if array[middle] > target:
+            upper = middle
+            continue
+        if array[middle] < target:
+            lower = middle
+            continue
+    
+    # When there is no more middle element to check, check the current left and right pointer elements
+    if array[lower] == target or array[upper] == target:
+        return True
+    
+    return False
+
+print(binarySearch([1,2, 7, 9], 7))
