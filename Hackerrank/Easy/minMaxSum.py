4f83023c9ed3d87c95ead22c838ac6890cbd2404
unknown <atwkdeveloper@outlook.com>
2019-12-06 23:39:59 +0800

    completed minmaxsum.py

---
+++ Hackerrank/Easy/minMaxSum.py
@@ -0,0 +1,24 @@
+#https://www.hackerrank.com/challenges/mini-max-sum/problem
+
+# Time: O(n) | Space: O(1)
+def miniMaxSum(arr):
+    min_num = float("inf")
+    max_num = float("-inf")
+    min_sum = 0
+    max_sum = 0
+    for i in range(len(arr)-1):
+        if arr[i] < min_num:
+            min_num = arr[i]
+        if arr[i] > max_num:
+            max_num = arr[i]
+        min_sum += arr[i]
+        max_sum += arr[i]
+
+    if arr[-1] > max_num:
+        max_sum -= min_num
+        max_sum += arr[-1]
+    if arr[-1] < min_num:
+        min_sum -= min_num
+        min_sum += arr[-1]
+
+    print(str(min_sum) + " " + str(max_sum))
