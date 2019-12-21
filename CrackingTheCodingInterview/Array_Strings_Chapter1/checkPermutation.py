24a05fb59aec327bb3bc3079f772eb24ff80aea4
unknown <atwkdeveloper@outlook.com>
2019-12-21 19:28:43 +0800

    solved 1.2

---
+++ CrackingTheCodingInterview/Array_Strings_Chapter1/checkPermutation.py
@@ -0,0 +1,36 @@
+
+# Given two strings , write a method to determine whether one if a permutation of another
+
+# Time: O(N Log N) - sorting | Space: O(N)
+def checkPermutation(a, b):
+
+    # Assumes that both strings are of the same length
+
+    arrA = list(a).sort()
+    arrB = list(b).sort()
+
+    return arrA == arrB
+
+# Time: O(N) | Space: O(N)
+def checkPermutation2(a, b):
+
+    dict_chars = {}
+
+    for char in a:
+        if char in dict_chars:
+            dict_chars[char] += 1
+        else:
+            dict_chars[char] = 1
+
+    for char in b:
+        if char not in dict_chars:
+            return False
+        else:
+            dict_chars[char] -= 1
+            if dict_chars[char] == -1:
+                return False
+    
+    return True
+    
+
+print(checkPermutation2('abca', 'cbab'))
