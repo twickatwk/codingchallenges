09e2286ca9713e12a402c7d9c555a9bc38d33bbe
unknown <atwkdeveloper@outlook.com>
2019-12-21 18:57:47 +0800

    solved 1.1

---
+++ CrackingTheCodingInterview/Array_Strings_Chapter1/isUnique.py
@@ -0,0 +1,32 @@
+
+# Develop a function that checks whether the string has all unique characters
+
+# Time: O(N) | Space: O(N) uses a dictionary
+def isUnique(string):
+    dict_characters = {}
+    unique = True
+    for char in string:
+        if char in dict_characters:
+            unique = False
+        else:
+            dict_characters[char] = True
+    
+    return unique
+
+# Time: O(N + Log N) | Space: O(1)
+def isUnique2(string):
+
+    arr = list(string)
+    # Time: O(Log N)
+    arr.sort()
+
+    unique = True
+
+    # Time: O(N)
+    for i in range(len(arr)-1):
+        if arr[i] == arr[i+1]:
+            unique = False
+    
+    return unique
+
+print(isUnique2('abx'))
