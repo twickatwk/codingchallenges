d50370bec56be96cb76bba87073af04a0ed192c1
unknown <atwkdeveloper@outlook.com>
2019-12-22 00:58:40 +0800

    added another optimal solution for lc easy

--- Leetcode/Easy/fibonacciNumbers.py
+++ Leetcode/Easy/fibonacciNumbers.py
@@ -1,4 +1,6 @@
+# https://leetcode.com/problems/fibonacci-number/
 
+# Easy
 
 # This approach uses recursion but its not optimal yet
 # Time: O(2^N) | Space: O(N)
@@ -15,4 +17,27 @@ def fib(N):
     
     return fib(N-1) + fib(N-2)
 
-print(fib(3))
+
+# This approach is the most optimal, for fib numbers that are previously calculated, you do not recalculate them, by maintaing a memo table
+# Time: O(N) - each fib number is only counted exactly once | Space: O(N)
+def fib2(N, prevNums=None):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if prevNums is None:
+            prevNums = {}
+        
+        if N == 1:
+            return 1
+        if N == 0:
+            return 0
+        
+        if N in prevNums:
+            return prevNums[N]
+        
+        prevNums[N] = fib2(N-1, prevNums) + fib2(N-2, prevNums)
+        
+        return prevNums[N]
+
+print(fib2(3))
