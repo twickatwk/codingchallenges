e6806d28387eb73817432a896787366e1e79155f
unknown <atwkdeveloper@outlook.com>
2020-01-03 22:54:42 +0800

    added a better solution

--- Leetcode/Easy/100SameTree.py
+++ Leetcode/Easy/100SameTree.py
@@ -1,4 +1,19 @@
 
+# Time: O(N) | Space: O(D / Log N) - depends on the depth of the recursion but it is still lesser than the approach below
+def isSameTree2(p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None or q is None:
+            return p == q
+        
+        if p.val != q.val:
+            return False
+        
+        return isSameTree2(p.left, q.left) and isSameTree2(p.right, q.right)
+
 # Time: O(N) | Space: O(N) - Space can still be reduced
 def isSameTree(p, q):
         """
