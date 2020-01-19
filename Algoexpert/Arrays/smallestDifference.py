74f1de5f3e9d892248d5daf3ca4b47e8c5f5612a
unknown <atwkdeveloper@outlook.com>
2020-01-19 16:14:14 +0800

    added another solution

--- Algoexpert/Arrays/smallestDifference.py
+++ Algoexpert/Arrays/smallestDifference.py
@@ -15,5 +15,29 @@ def smallestDifference(arrayOne, arrayTwo):
 				result = [num1, num2]
 	return result
 
-
-
+# Approach 2: Uses sorted arrays
+# Time: O(N) | Space: O(1)
+def smallestDifference2(arrayOne, arrayTwo):
+    # Write your code here.
+	
+	arrayOne.sort()
+	arrayTwo.sort()
+	
+	aIdx = 0
+	bIdx = 0
+	
+	minDifference = float("inf")
+	result = []
+	
+	while aIdx < len(arrayOne) and bIdx < len(arrayTwo):
+		num1 = arrayOne[aIdx]
+		num2 = arrayTwo[bIdx]
+		difference = abs(num1 - num2)
+		if difference < minDifference:
+			minDifference = difference
+			result = [num1, num2]
+		if num1 <= num2:
+			aIdx += 1
+		else:
+			bIdx += 1
+	return result
