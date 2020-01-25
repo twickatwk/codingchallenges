73f1a943f288edb100c26c899e503a6f5e91e1c6
unknown <atwkdeveloper@outlook.com>
2020-01-25 21:16:14 +0800

    added a more efficient solution

--- Algoexpert/Binary Search Trees/findClosestValueInBST.py
+++ Algoexpert/Binary Search Trees/findClosestValueInBST.py
@@ -1,6 +1,35 @@
-# Recursive Approach
-# Average Time: O(Log N) | Space: O(D)
+# Iterative Approach
+# Average Time: O(Log N) | Worst Time: O(N) | Space: O(1)
 def findClosestValueInBst(tree, target):
+    # Write your code here.
+
+    if tree.left is None and tree.right is None:
+        return tree.value
+
+    result = tree.value
+    minDifference = abs(target-tree.value)
+
+    while tree is not None:
+        
+        difference = abs(target-tree.value)
+        
+        if difference == 0:
+            return tree.value
+        
+        if difference < minDifference:
+            minDifference = difference
+            result = tree.value
+        
+        if tree.value < target:
+            tree = tree.right
+        else:
+            tree = tree.left
+
+    return result
+
+# Recursive Approach
+# Average Time: O(Log N) | Worst Time: O(N) | Space: O(D) - Call stack
+def findClosestValueInBst2(tree, target):
     # Write your code here.
 	
     result, minDifference = traverseTree(tree, target, result=None, minDifference=None)
