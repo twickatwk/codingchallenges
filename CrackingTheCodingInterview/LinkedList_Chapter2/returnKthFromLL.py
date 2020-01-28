cb431425b2b32a42dce45cec98917acbea63b5c1
unknown <atwkdeveloper@outlook.com>
2020-01-29 00:51:20 +0800

    added another solution

--- CrackingTheCodingInterview/LinkedList_Chapter2/returnKthFromLL.py
+++ CrackingTheCodingInterview/LinkedList_Chapter2/returnKthFromLL.py
@@ -18,11 +18,22 @@ def returnKthFromLast(head, k):
         print(head.data)
 
     return index
+# This approach assumes that the size of the linked list is given
+# Time; O(N) | Space:O(N)
+def returnKthFromLast2(head, size, k):
+
+    kthElement = size - k
+
+    for i in range(kthElement):
+        head = head.next
+
+    return head.data
 
 head = Node('a')
 head.next = Node('b')
 head.next.next = Node('c')
 head.next.next.next = Node('d')
-
-# Expected: 'c'
+head.next.next.next.next = Node('e')
+# Expected: 'd'
 returnKthFromLast(head, 2)
+print(returnKthFromLast2(head, 5, 2))
