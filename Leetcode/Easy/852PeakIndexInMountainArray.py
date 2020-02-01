0f207670a30e22f79439f6afd5e7a0a9a84faf66
unknown <atwkdeveloper@outlook.com>
2020-02-01 19:41:06 +0800

    added another solution

---
+++ Leetcode/Easy/852PeakIndexInMountainArray.py
@@ -0,0 +1,29 @@
+
+
+
+
+def peakIndexInMountainArrayIndexInMountainArray(A):
+    """
+    :type A: List[int]
+    :rtype: int
+    """
+    maxPeakHeight = 0
+    maxPeakIndex = 0
+
+    for i in range(len(A)):
+        largerThanLeft = True
+        largerThanRight = True
+
+        if i-1 >= 0 and A[i] <= A[i-1]:
+            largerThanLeft = False
+        if i+1 < len(A) and A[i] <= A[i+1]:
+            largerThanRight = False
+
+        if largerThanLeft and largerThanRight:
+            maxPeakHeight = A[i]
+            maxPeakIndex = i
+
+    return maxPeakIndex
+
+
+print(peakIndexInMountainArrayIndexInMountainArray([0,10]))
