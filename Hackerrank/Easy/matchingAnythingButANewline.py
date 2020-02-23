8743df75b3fb662554ae25892c69b46d408bcf19
unknown <atwkdeveloper@outlook.com>
2020-02-23 14:50:54 +0800

    added another question

---
+++ Hackerrank/Easy/matchingAnythingButANewline.py
@@ -0,0 +1,19 @@
+
+# write a method that gets a string in the form of abc.def.ghi.jkx
+# where abcdefghijkx can be any single character except the newline
+
+import re
+
+def getMatch(string):
+
+    re_pattern = r'...\....\....\....'
+
+    match = re.match(re_pattern, string)
+
+    print(match is not None)
+
+
+text = "123.456.abc.def"
+text2 = "1123.456.abc.def"
+getMatch(text2)
+
