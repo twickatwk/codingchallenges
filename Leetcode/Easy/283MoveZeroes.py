f4401973be55af43ff3a030c663f366cf9a1246e
unknown <atwkdeveloper@outlook.com>
2020-04-04 16:15:52 +0800

    modified solution

--- Leetcode/Easy/283MoveZeroes.py
+++ Leetcode/Easy/283MoveZeroes.py
@@ -6,24 +6,23 @@ def moveZeroes(nums):
     """
     
     start = 0
-    end = None
+    end = start
     
-    while start < len(nums)-1:
+    while start < len(nums):
+        # keep finding till you find the first 0 for start
         while nums[start] != 0:
             start += 1
-            if start == len(nums) - 1:
+            # if start reaches the end of the array, simply stop, and return
+            if start == len(nums):
                 return nums
-            
-        end = start + 1
-
+        end = start
         while nums[end] == 0:
             end += 1
+            # if end reaches the end of array, simply stop, and return
             if end == len(nums):
                 return nums
             
         nums[start], nums[end] = nums[end], nums[start]
-        
-        
         start += 1
-    
+          
     return nums
