ef9e8213197d3aab5248351c9a115f1b0ac5c6d7
unknown <atwkdeveloper@outlook.com>
2020-04-05 11:58:09 +0800

    added another example for datacamp

--- Datacamp/Operators.py
+++ Datacamp/Operators.py
@@ -1,3 +1,5 @@
+########################################################################################
+
 # Example: Logical Operators: Numpys - logical_or/and/not
 import numpy as np
 my_house = np.array([18.0, 20.0, 10.75, 9.50])
@@ -8,3 +10,22 @@ print(np.logical_or(my_house<10, my_house>18.5))
 
 # Both my_house and your_house smaller than 11
 print(np.logical_and(my_house<11, your_house<11))
+
+########################################################################################
+
+# Example: Logical Operators: DataFrame
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# 1 - select column from DataFrame, 2 - filter series using np logical operators, 3 - subsetting in DF
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 100, cpc < 500)
+medium = cars[between]
+
+# Print medium
+print(medium)
