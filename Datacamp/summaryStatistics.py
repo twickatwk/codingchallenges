2b790ebd4c5de913a928ab63abbf7d3002cc8307
unknown <atwkdeveloper@outlook.com>
2020-04-07 20:32:49 +0800

    modified example for summary statistics

--- Datacamp/summaryStatistics.py
+++ Datacamp/summaryStatistics.py
@@ -1,9 +1,16 @@
 
 # Summary Statistics Example Code for Walmart Data
 import numpy as np
+import pandas as pd
+
+sales = pd.read_csv('./datasets/walmart_ds.csv')
+
 def iqr(column):
     return column.quantile(0.75) - column.quantile(0.25)
 
 # sales is the DataFrame that is obtained from Walmart Dataset
 # Update to print IQR and median of temperature_c, fuel_price_usd_per_l, & unemployment
-print(sales[["temperature_c", "fuel_price_usd_per_l", "unemployment"]].agg([iqr, np.median]))
+print(sales.head())
+print()
+print("########################Summary Statistics########################################")
+print(sales[["Temperature", "Fuel_Price", "Unemployment"]].agg([iqr, np.median]))
